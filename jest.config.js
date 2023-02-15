18c21ae278c19fe4852e47958eb7ad35db612310
epheitea <ephei@outlook.com>
2023-02-15 22:40:41 +0700

    First Jest test passed

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    transform: {
+        "^.+\\.(js|jsx)$": "babel-jest",
+    },
+    testMatch: ['**/components/*.cy.js?(x)'],
+    testEnvironment: 'jest-environment-jsdom'
+};
--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import RestaurantScreen from './components/RestaurantScreen';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <a> This is a change.</a>
-
-      <a> Change for Lab 3 </a>
-
-      </header>
-    </div>
+    <div>
+      <RestaurantScreen />
+    </div >
   );
 }
-
-export default App;
