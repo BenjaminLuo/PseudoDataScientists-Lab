760676efeea4feb2875df77fc3ccf1276bf9dea9
f237khan <f237khan@uwaterloo.ca>
2023-03-20 11:25:20 -0400

    fixed baseURL

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 const client = axios.create({
-baseURL: 'https://api.outsidein.dev/exxbCrbkA4UFQtubQsBKR5aX2DCN04j2',
+baseURL: 'https://api.outsidein.dev/exxbCrbkA4UFQtubQsBKR5aX2DCN04j2/restaurants',
 });
 const api = {
     async loadRestaurants() {
         const response = await client.get('/restaurants');
+        console.log(response.data);
         return response.data;
         },
     };
